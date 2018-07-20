0bb625ddcc7a4806ef7e3f2890ece805e549bd99
yangzonglin <806765278@qq.com>
2018-07-20 23:36:27 +0800

    1、raw_data目录和processed_data目录。 2、处理单引号。 3、预处理原始训练数据，使得变为标准格式。 4、获得训练数据的词汇表。 5、新增各个函数的测试代码。

--- params.py
+++ params.py
@@ -1,21 +1,31 @@
 import os
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATA_PATH = os.path.join(PROJECT_ROOT, 'data')
 
-CIKM_ENGLISH_TRAIN_DATA = os.path.join(DATA_PATH, 'cikm_english_train_20180516.txt')
-CIKM_SPANISH_TRAIN_DATA = os.path.join(DATA_PATH, 'cikm_spanish_train_20180516.txt')
-CIKM_TEST_DATA = os.path.join(DATA_PATH, 'cikm_test_a_20180516.txt')
-CIMK_UNLABELED_SPANISH_DATA = os.path.join(DATA_PATH, 'cikm_unlabel_spanish_train_20180516.txt')
-
-fastText_EN_PRE_TRAINED_WIKI_WORD_VEC = os.path.join(DATA_PATH,
+# raw data
+RAW_DATA_PATH = os.path.join(PROJECT_ROOT, 'data', 'raw_data')
+CIKM_ENGLISH_TRAIN_DATA = os.path.join(RAW_DATA_PATH, 'cikm_english_train_20180516.txt')
+CIKM_SPANISH_TRAIN_DATA = os.path.join(RAW_DATA_PATH, 'cikm_spanish_train_20180516.txt')
+CIKM_TEST_DATA = os.path.join(RAW_DATA_PATH, 'cikm_test_a_20180516.txt')
+CIMK_UNLABELED_SPANISH_DATA = os.path.join(RAW_DATA_PATH, 'cikm_unlabel_spanish_train_20180516.txt')
+fastText_EN_PRE_TRAINED_WIKI_WORD_VEC = os.path.join(RAW_DATA_PATH,
                                                      'fast_text_vectors_wiki.en.vec',
                                                      'wiki.en.vec')
-fastText_ES_PRE_TRAINED_WIKI_WORD_VEC = os.path.join(DATA_PATH,
+fastText_ES_PRE_TRAINED_WIKI_WORD_VEC = os.path.join(RAW_DATA_PATH,
                                                      'fast_text_vectors_wiki.es.vec',
                                                      'wiki.es.vec')
+
+# processed data
+PROCESSED_DATA_PATH = os.path.join(PROJECT_ROOT, 'data', 'processed_data')
+PROCESSED_EN_TRAIN_DATA = os.path.join(PROCESSED_DATA_PATH, 'processed_en_train')
+PROCESSED_ES_TRAIN_DATA = os.path.join(PROCESSED_DATA_PATH, 'processed_es_train')
+PROCESSED_EN_WORD_VEC = os.path.join(PROCESSED_DATA_PATH, 'processed.wiki.en.vec')
+PROCESSED_ES_WORD_VEC = os.path.join(PROCESSED_DATA_PATH, 'processed.wiki.es.vec')
 
 OPEN_FILE_ENCODING = 'utf-8'
 SAVE_FILE_ENCODING = 'utf-8'
 
-BATCH_SAMPLES_NUMBER = 256  # 32 64 128 256
+BATCH_SAMPLES_NUMBER = 32  # 32 64 128 256
+
+SPLIT_PATTERN_STR = r'[ ,.;"?():!/]+'
+MATCH_SINGLE_QUOTE_STR = r'[^a-zA-Z]*(\')[a-zA-Z ]+(\')[^a-zA-Z]+'
--- reader.py
+++ reader.py
@@ -1,15 +1,20 @@
 import io
+import re
+
+import nltk
+
 import params
 
 
-def load_vectors(filename, head_n=None):
+def _load_vectors(filename, head_n=None):
     """
     装载前N个词向量
     :param filename:
     :param head_n: head n word vectors will be loaded
     :return: dict, {word: str => vector: float list}
     """
-    fin = io.open(filename, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    fin = io.open(filename, 'r', encoding=params.OPEN_FILE_ENCODING,
+                  newline='\n', errors='ignore')
     line_count = 0
     data = {}
     for line in fin:
@@ -27,10 +32,95 @@ def load_vectors(filename, head_n=None):
     return data
 
 
+def _remove_symbols(seq, pattern_str):
+    """
+    remove specified symbol from seq
+    :param seq:
+    :param pattern_str: 例如text-preprocess项目的remove_comma_from_number()
+    :return: new seq
+    """
+    match_symbol_pattern = re.compile(pattern_str)
+    while True:
+        matched_obj = match_symbol_pattern.search(seq)
+        if matched_obj:
+            matched_str = matched_obj.group()
+            # print('matched_str:', matched_str)
+            matched_symbol = matched_obj.group(1)
+            seq = seq.replace(matched_str, matched_str.replace(matched_symbol, ''))
+        else:
+            break
+    return seq
+
+
+def _pre_process(src_fname, tgt_fname):
+    """
+    transform raw train data to standard format
+    save them to tgt_file
+    in lower case
+    :return: None
+    """
+    with open(src_fname, 'r', encoding=params.OPEN_FILE_ENCODING) as src_file, \
+            open(tgt_fname, 'w', encoding=params.SAVE_FILE_ENCODING) as tgt_file:
+        for line in src_file:
+            field_list = line.split('\t')
+            for sentence in [field_list[0], field_list[2]]:
+                sentence = _remove_symbols(sentence, params.MATCH_SINGLE_QUOTE_STR)
+                tgt_file.write(sentence.lower())
+                tgt_file.write('\t')
+            tgt_file.write(field_list[4])
+
+
+def _read_words(fname):
+    """
+    read all distinct words
+    :param fname:
+    :return: set, {'apple', 'banana', ...}
+    """
+    ret_words = set()
+    split_pattern = re.compile(params.SPLIT_PATTERN_STR)
+    with open(fname, 'r', encoding=params.OPEN_FILE_ENCODING) as file:
+        for line in file:
+            field_list = line.split('\t')
+            source1 = field_list[0]
+            source2 = field_list[1]
+            for word in split_pattern.split(source1) + split_pattern.split(source2):
+                if word:
+                    ret_words.add(word)
+    return ret_words
+
+
+def _get_needed_vectors():
+    pass
+
 if __name__ == '__main__':
-    word2vec = load_vectors(params.fastText_EN_PRE_TRAINED_WIKI_WORD_VEC, head_n=4)
-    for word, vector in word2vec.items():
-        print(word, end=' ')
-        for value in vector:
-            print(value, end=' ')
-        print()
+    # # ========== test _load_vectors() function ==========
+    # word2vec = _load_vectors(params.fastText_EN_PRE_TRAINED_WIKI_WORD_VEC, head_n=4)
+    # for word, vector in word2vec.items():
+    #     print(word, end=' ')
+    #     for value in vector:
+    #         print(value, end=' ')
+    #     print()
+
+    # # ========== test _remove_symbols() func ==========
+    # # str1 = "'Random Number' is what I don't like at all."
+    # # str1 = "I don't like 'Random Number'."
+    # str1 = "I don't like 'Random Number' at all"
+    # print(_remove_symbols(str1, params.MATCH_SINGLE_QUOTE_STR))
+
+    # # ========== test _pre_process() func ==========
+    # _pre_process(params.CIKM_ENGLISH_TRAIN_DATA, params.PROCESSED_EN_TRAIN_DATA)
+
+    # # ========== test _read_words() function ==========
+    # all_distinct_words = _read_words(params.PROCESSED_EN_TRAIN_DATA)
+    # for word in all_distinct_words:
+    #     print(word)
+    # print('total distinct words number:', len(all_distinct_words))
+
+    # ========== test NLTK ==========
+    sentence = "I don't like 'Random Number' when time is eight o'clock."
+    tokens = nltk.word_tokenize(sentence)
+    for token in tokens:
+        print(token, end=' ')
+    print()
+
+    # ========== other test ==========
